bb456bda30d25d21171ff81cac66faa0da53ebc9
dgraux <damien.graux@inria.fr>
2016-08-09 11:12:19 +0200

    Option --debug

--- eval.sh
+++ eval.sh
@@ -2,16 +2,32 @@
 
 PATH_CMD=$(dirname $0)
 
+debug=0
+while true; do
+    case "$1" in
+	"--debug" ) debug=1; shift ;;
+	* ) break ;;
+    esac
+done
+
 if [[ $# != 2 ]];
 then
-    echo "Usage: $0 dbName queryLocalPath"
+    echo "Usage: $0 [--debug] dbName queryLocalPath"
     exit 1
 fi
 dbName=$1
 queryFile=$2
-
 sql=$(bash ${PATH_CMD}/translate2sql.sh $dbName $queryFile)
-
+if [[ $debug == 1 ]];
+then
+    echo "=== SPARQL Query ==="
+    cat $queryFile
+    echo ""
+    echo "=== SQL Query ==="
+    echo $sql
+    echo ""
+    sleep 2
+fi
 hive -e "$sql ;"
 
 exit 0
