a6f14cdc428c5a98903908409a7ff582b33fceda
dgraux <damien.graux@inria.fr>
2017-03-14 10:29:07 +0100

    Un chargement beaucoup plus rapide

--- bin/load.sh
+++ bin/load.sh
@@ -7,10 +7,12 @@ then
     exit 1;
 fi
 tbeg=$(date +%s)
-hive -e "CREATE DATABASE IF NOT EXISTS $1 ;" &> $log
-hive -e "USE $1 ;" &> $log
-hive -e "CREATE EXTERNAL TABLE $1.triples (subj string, pred string, obj string) row format delimited fields terminated by ' ' stored as textfile ;" &> $log
-hive -e "LOAD DATA INPATH '$2' OVERWRITE INTO TABLE $1.triples ;" &> $log
+echo "CREATE DATABASE IF NOT EXISTS $1 ;" > script.tmp
+echo "USE $1 ;" >> script.tmp
+echo "CREATE EXTERNAL TABLE $1.triples (subj string, pred string, obj string) row format delimited fields terminated by ' ' stored as textfile ;" >> script.tmp
+echo "LOAD DATA INPATH '$2' OVERWRITE INTO TABLE $1.triples ;" >> script.tmp
+hive -f script.tmp &> $log
 tend=$(date +%s)
 echo "$1 (at $2 on HDFS) loaded in $(($tend-$tbeg)) seconds."
-exit 0;
+rm script.tmp
+exit 0
